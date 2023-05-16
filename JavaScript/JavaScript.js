5cb1d5f14efcdfbc5a07f95cacaa268890396231
thetensa <133793638+thetensa@users.noreply.github.com>
2023-05-16 21:51:10 +0300

    Add files via upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,9 @@
+﻿document.getElementById("baslik").style.fontSize = "60";
+document.getElementById("baslik").style.backgroundColor = "red";
+document.getElementById("baslik").style.color = "yellow";
+document.getElementById("baslik").innerHTML = "Javascript Uygulaması";
+
+document.getElementById("paragraf").style.fontSize = "20";
+document.getElementById("paragraf").style.color = "blue";
+
+document.getElementById("adbilgi").value ="Adınız Nedir ?";
